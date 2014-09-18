8ca5d4fac9f4b2d6d394b4b7892945c223917e52
Slim <luccy559slim@gmail.com>
2014-09-18 18:02:43 +0100

    Improvements

--- js/app.js
+++ js/app.js
@@ -2,51 +2,95 @@ var lyricHub = {
 
 	jsonResult:null,
 	searchButton: null,
+	resetButton: null,
 	artistField:null,
 	songField:null,
 	displayArea:null,
 	params: {fmt: 'realjson'},
+	params1: {client_id: 'a5d568cd2894d82c7060a31ab7a67348'},
 	base: "https://lyrics.wikia.com/api.php?callback=?",
+	base1: "https://api.soundcloud.com/tracks.json",
 	init:function()
 	{
 		lyricHub.searchButton = $("#searchButton");
+		lyricHub.resetButton = $("#resetButton");
 		lyricHub.artistField = $("#artistField");
 		lyricHub.songField = $("#songField");
 		lyricHub.displayArea = $("#displayArea");
+		lyricHub.ifRelatedSongs = $("#ifRelatedSongs");
+		lyricHub.audioPlayer = $("#player");
 		lyricHub.initEvent();
 	},
 	//INITIALIZE EVENTS
 	initEvent:function()
 	{
+		lyricHub.resetButton.click(function () {
+			$("#songField").val("");
+			$("#artistField").val("");
+		});
 		lyricHub.searchButton.click(lyricHub.onSearch);
+		$("#loading").fadeOut();
+		lyricHub.configureAudioPlayer();
+		lyricHub.ifRelatedSongs.on("click" ,"a#playSong", lyricHub.playSounds);
 	},
 	onSearch:function(e)
 	{	
 		var artist, song;
-		song = $("#songField").val().trim();
-		artist = $("#artistField").val().trim();
+		song = $("#songField").val().trim().toLowerCase();
+		artist = $("#artistField").val().trim().toLowerCase();
 		if(artist === "" && song === "") {
 			lyricHub.displayArea.text('You must enter at least one parameter');
-		} else {
-			if(song !== "") 
-			{
-				lyricHub.params.song = song;
-			}
-			if(artist !== "") {
-				lyricHub.params.artist = artist;
-			}
+		} 
+		else if(song !== "" && artist !== "")  
+		{
+			lyricHub.params.song = song;
+			lyricHub.params.artist = artist;
+			lyricHub.params1.q = song;
 			lyricHub.searchFind();
+			lyricHub.findSongs();
+		}
+		else {
+			if(artist !== "")
+			{
+				lyricHub.params.artist = artist;
+				lyricHub.params1.q = artist;
+				lyricHub.searchFind();
+				lyricHub.findSongs();	
+			}
+			else
+			{
+				lyricHub.params1.q = song;
+				lyricHub.findSongs();
+			}
 		}
 			
 		e.preventDefault();
 	},
+	configureAudioPlayer: function()
+	{
+		lyricHub.audioPlayer = $("audio#player").get(0);
+		lyricHub.audioPlayer.loop = true;
+	},
+
 	//find song lyrics from LyricWiki API
 	searchFind: function()
 	{
 		console.log(lyricHub.params);
 		$.getJSON(lyricHub.base, lyricHub.params, function(response) {
 			console.log(response);
+			$("#loading").fadeIn();
 			lyricHub.onSuccess(response);
+		});
+	},
+
+	//find Related songs from SoundCloud API
+	findSongs: function()
+	{
+		console.log(lyricHub.params1);
+		$.getJSON(lyricHub.base1, lyricHub.params1, function(songs) {
+			console.log(songs);
+			$("#loading").fadeIn();
+			lyricHub.onFindSuccess(songs);
 		});
 	},
 		
@@ -55,14 +99,47 @@ var lyricHub = {
 	{
 		lyricHub.jsonResult = response;
 		console.log(lyricHub.jsonResult);
-		$("#artist").text(lyricHub.jsonResult.artist);
-		$("#song").text(lyricHub.jsonResult.song);
-		$("#lyric").text(lyricHub.jsonResult.lyrics);
-		$("#link").text(lyricHub.jsonResult.url);
-
+		$("#loading").fadeOut();
+		$("#artist").text("Artist : " + lyricHub.jsonResult.artist);
+		$("#song").text("Song Title : " + lyricHub.jsonResult.song);
+		$("#lyric").text("Song Lyrics : " + lyricHub.jsonResult.lyrics);
+		$("#link").text("Continue to full lyrics...");
+		$("#link").attr("href", lyricHub.jsonResult.url);
 		
 	},
+	onFindSuccess: function(response)
+	{
+		lyricHub.jsonResult = response;
+		var songListArea = "";
+		console.log(lyricHub.jsonResult);
+		$("#loading").fadeOut();
+		$.each(response, function() {
+			var songList = this;
+			var caption = songList.title;
+			var image = songList.artwork_url;
+			if (image == undefined) {
+				image = "img/albumArt.jpg";
+			}
+			var listenToSong = this.stream_url + '?client_id='+ lyricHub.params1.client_id;
+			songListArea += '<div class="songFound">' +
+							'<ul>' +
+							'<li><img src="' + image + '"/ class="albumArt"></li>' +
+							'<li><a href="' + listenToSong + '" id="playSong" target="_blank"><img src="img/play.png" class="playIcon"/></a></li>' +
+							'<li><p>' + caption +'</p></li>' +
+							'</ul>' 
+							'</div>';
+			$('#ifRelatedSongs').html(songListArea);
+		});
+	},
 
-
+	playSounds: function(e)
+	{
+		e.preventDefault();
+		var href = $(this).prop("href");
+		lyricHub.audioPlayer.src = href;
+		console.log(href);
+		lyricHub.audioPlayer.play();
+		return false;
+	}
 }
 $(document).ready(lyricHub.init)
